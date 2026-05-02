b02e27a6f2a7ffe15aed0e07eb292b444a7256cd
pimentajuliano52-png <pimentajuliano52@gmail.com>
2026-05-02 00:27:18 -0300

    Atualizar o index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,52 @@
+import { useState } from "react";
+import Avatar from "../components/Avatar";
+
 export default function Home() {
+
+  const [xp, setXp] = useState(0);
+  const [skills, setSkills] = useState([]);
+
+  function adicionarSkill(nome, valorXp) {
+    if (!skills.includes(nome)) {
+      setSkills([...skills, nome]);
+      setXp(xp + valorXp);
+    }
+  }
+
   return (
-    <div>
-      <h1>Protheus Journey</h1>
-      <p>App rodando 🚀</p>
+    <div style={{ padding: 20 }}>
+
+      <h1>🚀 Protheus Journey</h1>
+
+      <Avatar xp={xp} />
+
+      <h2>XP: {xp}</h2>
+
+      <h3>Skills:</h3>
+
+      <button onClick={() => adicionarSkill("Criar tabela", 50)}>
+        Sei criar tabela (+50 XP)
+      </button>
+
+      <br /><br />
+
+      <button onClick={() => adicionarSkill("ADVPL básico", 100)}>
+        Sei ADVPL básico (+100 XP)
+      </button>
+
+      <br /><br />
+
+      <button onClick={() => adicionarSkill("SELECT SQL", 80)}>
+        Sei SELECT SQL (+80 XP)
+      </button>
+
+      <h3>Skills adquiridas:</h3>
+      <ul>
+        {skills.map((s, i) => (
+          <li key={i}>{s}</li>
+        ))}
+      </ul>
+
     </div>
   );
 }
